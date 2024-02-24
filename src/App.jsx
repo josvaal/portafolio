ce642f9ee10fda1d54ce535d86bc6aa9d5e21962
heyy-josval <josvalmtrz@gmail.com>
2024-02-24 12:02:19 -0500

    god pls work

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,38 @@ import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
 import { Route, Switch } from "wouter";
 import Experiences from "./pages/Experiences";
+import { Router } from "wouter";
+import { useLocation } from "wouter";
 
 const App = () => {
+  const [location, setLocation] = useLocation();
+  console.log(location);
   return (
     <div className="max-w-[45rem] m-auto px-6">
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/experience" component={Experiences} />
-      </Switch>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Router base="">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/experience" component={Experiences} />
+            <Route>
+              {() => {
+                console.log(location);
+                setLocation("/");
+              }}
+            </Route>
+            <Route path="*">
+              {() => {
+                console.log(location);
+                setLocation("/");
+              }}
+            </Route>
+          </Switch>
+        </Router>
+      </main>
       <Footer />
     </div>
   );
