4f368680d18455c1d1f0a903be45c3cdeebd626e
José Valentino <josvalmtrz@gmail.com>
2024-08-24 04:08:44 -0500

    feat: Coumn view icon tech

--- src/components/IconTech.jsx
+++ src/components/IconTech.jsx
@@ -1,16 +1,13 @@
-
 // eslint-disable-next-line react/prop-types
 function IconTech({ text, icon }) {
   return (
-    <div className="h-full has-tooltip px-4 pb-2 pt-4 rounded-3xl text-xl font-light  hover:text-red-400 transition-all duration-300 flex gap-2 items-center justify-center">
-      <div className="scale-150">
-        {icon}
-      </div>
-      <span className="tooltip rounded shadow-lg py-1 px-2 bg-[rgba(0,0,0,0.1)] text-white -mt-16 text-sm">
+    <div className="h-full px-4 pb-2 pt-4 rounded-3xl text-xl font-light  hover:text-red-400 transition-all duration-300 flex flex-col gap-2 items-center justify-center">
+      <div className="scale-150">{icon}</div>
+      <p className="rounded  py-1 px-2  text-white text-sm opacity-50">
         {text}
-      </span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default IconTech
+export default IconTech;
