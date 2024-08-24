03a2d4e1444c20064a753282e41a4412e57e1b81
José Valentino <josvalmtrz@gmail.com>
2024-08-24 04:08:32 -0500

    feat: Modified the experience module

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,12 +2,17 @@
 // eslint-disable-next-line react/prop-types
 const Experience = ({ job, company, company_link, date, descriptions }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-7 z-50 gap-10">
+    <div className="grid grid-cols-1 relative md:grid-cols-7 z-50 gap-10">
+      <div className="absolute -left-7 top-0 bottom-0 flex flex-col items-center">
+        <div className="w-1 bg-red-400 h-full"></div> {/* Línea vertical */}
+        <div className="w-4 h-4 bg-white rounded-full absolute -left-1.5 "></div>{" "}
+        {/* Punto */}
+      </div>
       <div className="md:col-span-3 pb-5">
         <h1 className="text-xl font-bold text-balance text-red-400">{job}</h1>
         <div className="pb-1">
           <a
-            className="text-xl font-bold text-balance hover:opacity-75 transition-all"
+            className="text-2xl font-bold text-balance hover:opacity-75 transition-all"
             href={company_link}
             target="_blank"
             rel="noreferrer"
